f43d5ecb075f366d09a0910a49b8144c36bd63a7
andrepcb <136086118+andrepcb@users.noreply.github.com>
2023-08-01 09:15:09 +0100

    test: unit test for util.go (#65)
    * test: unit test for util.go
    
    * fix: addressing comments
    
    * change to fit recommendations
    
    * fixed for loops and tidied TestParseEndpointWithFallbackProtocol

---
+++ pkg/util/util_test.go
@@ -0,0 +1,349 @@
+package util
+
+import (
+	"context"
+
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"testing"
+	"time"
+)
+
+type addressAndDialTests struct {
+	endpoint      string
+	address       string
+	hasDial       bool  // as we would only need to check if it's nil or not
+	expectedError error //  this is I think mandatory
+}
+
+type dialTest struct {
+	ctx    context.Context
+	addr   string
+	hasErr bool
+}
+
+/*parseEndpointWithFallbackProtocol Test struct*/
+type pewfpTest struct {
+	endpoint         string
+	fallbackProtocol string
+	protocol         string
+	addr             string
+	hasErr           bool
+}
+
+type parseEndpointTest struct {
+	endpoint    string
+	protocol    string
+	addr        string
+	expectedErr error
+}
+
+type cpuTest struct {
+	name     uint
+	isInList bool
+}
+
+type nodeTest struct {
+	nodeName string
+	isInList bool
+}
+
+type stringTest struct {
+	str      string
+	isInList bool
+}
+
+func TestGetAddressAndDialer(t *testing.T) {
+
+	testCases := []addressAndDialTests{
+		{
+			endpoint:      "https://google.com/",
+			address:       "",
+			hasDial:       false,
+			expectedError: fmt.Errorf("protocol %q not supported", "https"),
+		},
+		{
+			endpoint:      "unix://google.com",
+			address:       "",
+			hasDial:       true,
+			expectedError: nil,
+		},
+		{
+			endpoint:      "",
+			address:       "",
+			hasDial:       false,
+			expectedError: nil,
+		},
+		{
+			endpoint:      "tcp://google.com",
+			address:       "",
+			hasDial:       false,
+			expectedError: fmt.Errorf("only support unix socket endpoint"),
+		},
+	}
+
+	for _, want := range testCases {
+
+		addrGot, dialGot, errGot := GetAddressAndDialer(want.endpoint)
+
+		if want.hasDial {
+			assert.NotNil(t, dialGot)
+		}
+
+		assert.Equal(t, want.address, addrGot, "the expected address and the actual address are not equal")
+		assert.Equal(t, want.expectedError, errGot, "expected error and actual error are not equal")
+
+	}
+
+}
+
+func TestDial(t *testing.T) {
+
+	testCases := []dialTest{
+		{
+			ctx: func() context.Context {
+				nil, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+				return nil
+			}(),
+			addr:   "https://google.com",
+			hasErr: true,
+		},
+		{
+			ctx: func() context.Context {
+				nil, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				return nil
+			}(),
+			addr:   "",
+			hasErr: true,
+		},
+		{
+			ctx: func() context.Context {
+				nil, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+				defer cancel()
+				return nil
+			}(),
+			addr:   "not a valid URL:",
+			hasErr: true,
+		},
+	}
+
+	for _, want := range testCases {
+		dialGot, errGot := dial(want.ctx, want.addr)
+
+		if want.hasErr {
+			assert.NotNil(t, errGot)
+
+		}
+		assert.Nil(t, dialGot)
+	}
+}
+
+func TestParseEndpointWithFallbackProtocol(t *testing.T) {
+
+	testCases := []addressAndDialTests{
+		{
+			endpoint:      "https://google.com/",
+			address:       "",
+			hasDial:       false,
+			expectedError: fmt.Errorf("protocol %q not supported", "https"),
+		},
+		{
+			endpoint:      "unix://google.com/hi/there",
+			address:       "/hi/there",
+			hasDial:       true,
+			expectedError: nil,
+		},
+		{
+			endpoint:      "",
+			address:       "",
+			hasDial:       false,
+			expectedError: nil,
+		},
+		{
+			endpoint:      "tcp://google.com",
+			address:       "",
+			hasDial:       false,
+			expectedError: fmt.Errorf("only support unix socket endpoint"),
+		},
+	}
+	for _, want := range testCases {
+
+		addrGot, dialGot, errGot := GetAddressAndDialer(want.endpoint)
+
+		if want.hasDial {
+			assert.NotNil(t, dialGot)
+		}
+
+		assert.Equal(t, want.address, addrGot, "the expected address and the actual address are not equal")
+		assert.Equal(t, want.expectedError, errGot, "expected error and actual error are not equal")
+
+	}
+}
+func TestParseEndpoint(t *testing.T) {
+
+	testCases := []parseEndpointTest{
+		{
+			endpoint:    "tcp://www.google.com/",
+			protocol:    "tcp",
+			addr:        "www.google.com",
+			expectedErr: nil,
+		},
+		{
+			endpoint:    "unix://www.bing.com/hi",
+			protocol:    "unix",
+			addr:        "/hi",
+			expectedErr: nil,
+		},
+		{
+			endpoint:    "https://www.github.com/",
+			protocol:    "https",
+			addr:        "",
+			expectedErr: fmt.Errorf("protocol %q not supported", "https"),
+		},
+		{
+			endpoint:    "error",
+			protocol:    "",
+			addr:        "",
+			expectedErr: fmt.Errorf("using %q as endpoint is deprecated, please consider using full url format", "error"),
+		},
+		{
+			endpoint:    "",
+			protocol:    "",
+			addr:        "",
+			expectedErr: fmt.Errorf("using %q as endpoint is deprecated, please consider using full url format", ""),
+		},
+	}
+	for _, want := range testCases {
+		protoGot, addrGot, errGot := parseEndpoint(want.endpoint)
+
+		assert.Equal(t, want.protocol, protoGot, "the expected protocol and the actual protocol are not equal")
+		assert.Equal(t, want.addr, addrGot, "the expected address and the actual address are not equal")
+		assert.Equal(t, want.expectedErr, errGot, "the expected error and the actual error are not equal")
+	}
+}
+
+func TestCPUInCPUList(t *testing.T) {
+
+	cpuList := []uint{1222267, 811198, 0}
+
+	testCases := []cpuTest{
+
+		{
+			name:     1222267,
+			isInList: true,
+		},
+		{
+			name:     44,
+			isInList: false,
+		},
+	}
+
+	var nilList []uint = nil
+
+	for _, want := range testCases {
+
+		resultTrue := CPUInCPUList(want.name, cpuList)
+		assert.Equal(t, want.isInList, resultTrue, "One variable that is expected to be in the List or not be in the list was handeled incorrectly")
+
+		resultFalse := CPUInCPUList(want.name, nilList)
+		assert.Equal(t, false, resultFalse, "nilList should be null")
+	}
+
+}
+
+func TestNodeNameInNodeList(t *testing.T) {
+	nodeList := []corev1.Node{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "Node1",
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "Node2",
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "!",
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "",
+			},
+		},
+	}
+	var nilList []corev1.Node = nil
+	testCases := []nodeTest{
+		{
+			nodeName: "Node1",
+			isInList: true,
+		},
+		{
+			nodeName: "",
+			isInList: true,
+		},
+		{
+			nodeName: "?",
+			isInList: false,
+		},
+		{
+			nodeName: "node10",
+			isInList: false,
+		},
+	}
+
+	for _, want := range testCases {
+
+		resultTrue := NodeNameInNodeList(want.nodeName, nodeList)
+		assert.Equal(t, want.isInList, resultTrue, "One variable that is expected to be in the list or not be in the list was handeled incorrectly")
+
+		resultFalse := NodeNameInNodeList(want.nodeName, nilList)
+		assert.Equal(t, false, resultFalse, "nilList should be empty")
+	}
+
+}
+
+func TestStringInStringList(t *testing.T) {
+	itemList := []string{"aa", "b", "3", "5", "", "!"}
+	var nilList []string = nil
+
+	testCases := []stringTest{
+		{
+			str:      "aa",
+			isInList: true,
+		},
+		{
+			str:      "",
+			isInList: true,
+		},
+		{
+			str:      "!",
+			isInList: true,
+		},
+		{
+			str:      "hi",
+			isInList: false,
+		},
+		{
+			str:      "-",
+			isInList: false,
+		},
+	}
+	for _, want := range testCases {
+
+		resultTrue := StringInStringList(want.str, itemList)
+		assert.Equal(t, want.isInList, resultTrue, "One variable that is expected to be in the list or not be in the list was handeled incorrectly")
+
+		resultFalse := StringInStringList(want.str, nilList)
+		assert.Equal(t, false, resultFalse, "nilList should be empty")
+	}
+
+}
